b0f4f47f0e4e430a7314c4070312cfb1595ead9e
Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>
2024-07-21 20:06:55 -0400

    Init

---
+++ windows/Simple/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::Simple::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::Simple::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
